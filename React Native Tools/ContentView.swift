26b11aec4ceb9caeedf7f59713c825ab9531df40
Henry Paulino <henrypl360@gmail.com>
2021-02-02 13:26:21 -0500

    Initial Commit

---
+++ React Native Tools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  React Native Tools
+//
+//  Created by Henry Paulino on 2/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ React Native Tools/React_Native_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  React_Native_ToolsApp.swift
+//  React Native Tools
+//
+//  Created by Henry Paulino on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct React_Native_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
