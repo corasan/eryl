3b5d025bd7c022aa37118b10cb820152f010f9c5
Henry Paulino <henrypl360@gmail.com>
2021-02-02 15:34:46 -0500

    basic layout

--- React Native Tools/ContentView.swift
+++ React Native Tools/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+		NavigationView {
+			VStack(alignment: .leading) {
+				Text("Logs")
+					.font(.title)
+					.fontWeight(.bold)
+					.padding(.vertical, 10)
+				Text("CodePush logs")
+					.font(.title)
+					.fontWeight(.bold)
+					.padding(.vertical, 10)
+				
+				VStack(alignment: .leading) {
+					Text("Start")
+						.font(.title)
+						.fontWeight(.bold)
+						.padding(.vertical, 10)
+					Text("CodePush")
+						.font(.title)
+						.fontWeight(.bold)
+						.padding(.vertical, 10)
+					Text("Run App")
+						.font(.title)
+						.fontWeight(.bold)
+						.padding(.vertical, 10)
+				}
+				.padding(.top, 50)
+				Spacer()
+			}
+			.padding(.vertical, 40)
+			.frame(width: 240)
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+			.frame(width: 1240, height: 820, alignment: .center)
     }
 }
